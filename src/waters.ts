dc16f2fe19e5dcd0770abd4bb663416dcf0f580c
Hiroki Terashima <honchikun@gmail.com>
2024-09-18 14:28:03 -0700

    Extract function to simplify constructor in Waters

--- src/waters.ts
+++ src/waters.ts
@@ -10,19 +10,23 @@ export class Waters {
     this.simulation = simulation;
     this.group = this.simulation.draw.group();
     if (this.simulation.numWaterThisCycle === 4) {
-      for (let i = 0; i < 4; i++) {
-        const shiftX = i % 2 ? 0 : 15;
-        const shiftY = i < 2 ? shiftX + 5 : shiftX + 20;
-        const waterPlant = this.simulation.draw
-          .ellipse(8, 12)
-          .fill(this.simulation.WATER_COLOR)
-          .attr({ cx: 194 + shiftX, cy: 94 + shiftY });
-        const waterChloroplast = this.simulation.draw
-          .ellipse(16, 24)
-          .fill(this.simulation.WATER_COLOR)
-          .attr({ cx: 620 + 2 * shiftX, cy: 60 + 2 * shiftY });
-        this.group.add(waterPlant).add(waterChloroplast);
-      }
+      this.createWaters();
+    }
+  }
+
+  private createWaters(): void {
+    for (let i = 0; i < 4; i++) {
+      const shiftX = i % 2 ? 0 : 15;
+      const shiftY = i < 2 ? shiftX + 5 : shiftX + 20;
+      const waterPlant = this.simulation.draw
+        .ellipse(8, 12)
+        .fill(this.simulation.WATER_COLOR)
+        .attr({ cx: 194 + shiftX, cy: 94 + shiftY });
+      const waterChloroplast = this.simulation.draw
+        .ellipse(16, 24)
+        .fill(this.simulation.WATER_COLOR)
+        .attr({ cx: 620 + 2 * shiftX, cy: 60 + 2 * shiftY });
+      this.group.add(waterPlant).add(waterChloroplast);
     }
   }
 
